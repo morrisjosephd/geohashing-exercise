0adfe8db6574cfbec0c80f6b07e641b2f5983a6b
Joe Morris <morris.josephd@gmail.com>
2016-02-25 21:49:27 -0500

    initial commit - request made, crypto

--- api.js
+++ api.js
@@ -1,14 +1,16 @@
-express = require('express');
-app     = express();
+var express = require('express');
+var app = express();
 
-geoHash = require('./lib/geohash');
+var geoHash = require('./lib/geohash');
 
-app.get('/geohash', function(req, res, next) {
-  respond = function(data){
+app.get('/geohash', function (req, res) {
+
+  var respond = function (data) {
     res.json(data);
   };
 
   geoHash(req.query.lat, req.query.lon, respond);
+
 });
 
 console.log("api listening on 8000");
